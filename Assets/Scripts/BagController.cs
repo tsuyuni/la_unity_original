a7d5bf2e3be77a97a5f5a221bf5ad294cefb9322
梅雨 <yuri.tsuchikawa@gmail.com>
2023-03-03 04:45:15 +0900

    アイテムの取得と画像のセットを実装

---
+++ Assets/Scripts/BagController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BagController : MonoBehaviour
+{
+
+    public static List<GameObject> possessingItems = new List<GameObject>();
+    public GameObject itemList;
+    public GameObject[] slots;
+    public static GameObject[] staticSlots;
+    public Sprite sourseSprite;
+    public static Sprite staticSourseSprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        staticSlots = slots;
+        staticSourseSprite = sourseSprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickBag(){
+        itemList.SetActive(!itemList.activeSelf);
+    }
+
+    public static void setItemImage(){
+        foreach(GameObject slot in staticSlots){
+            Image image = slot.GetComponent<Image>();
+            if(image.sprite == null){
+                image.sprite = staticSourseSprite;
+                image.color = new Color32(255,255,255,255);
+                Debug.Log("change");
+                break;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float speed = 3;
+    public GameObject camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+        if(Input.GetKey(KeyCode.W)){
+            gameObject.transform.position += gameObject.transform.rotation * Vector3.forward * speed * Time.deltaTime;
+        }
+
+        if(Input.GetKey(KeyCode.S)){
+            gameObject.transform.position -= gameObject.transform.rotation * Vector3.forward * speed * Time.deltaTime;
+        }
+
+        if(Input.GetKey(KeyCode.D)){
+            gameObject.transform.position += gameObject.transform.rotation * Vector3.right * speed * Time.deltaTime;
+        }
+
+        if(Input.GetKey(KeyCode.A)){
+            gameObject.transform.position -= gameObject.transform.rotation * Vector3.right * speed * Time.deltaTime;
+        }
+
+        if(Input.GetKey(KeyCode.UpArrow)){
+            if((camera.transform.localEulerAngles.x >= 345 || camera.transform.localEulerAngles.x <= 20)){
+                camera.transform.Rotate(-speed, 0, 0);
+            }
+        }
+
+        if(Input.GetKey(KeyCode.DownArrow)){
+            if((camera.transform.localEulerAngles.x >= 340 || camera.transform.localEulerAngles.x <= 15)){
+                camera.transform.Rotate(speed, 0, 0);
+            }
+        }
+
+        if(Input.GetKey(KeyCode.LeftArrow)){
+            gameObject.transform.Rotate(0, -speed, 0, Space.World);
+        }
+
+        if(Input.GetKey(KeyCode.RightArrow)){
+            gameObject.transform.Rotate(0, speed, 0, Space.World);
+        }
+
+
+
+        
+    }
+}
