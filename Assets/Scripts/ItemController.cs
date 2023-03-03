bbebdb7d2a664c27cacdd63882bb0ed11551f1c7
梅雨 <yuri.tsuchikawa@gmail.com>
2023-03-03 12:50:14 +0900

    fungusの導入及びログの作成

--- Assets/Scripts/ItemController.cs
+++ Assets/Scripts/ItemController.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Fungus;
 
 public class ItemController : MonoBehaviour
 {
+    Dictionary<string,bool> flags;
+    public Flowchart ResultLog = null;
     // Start is called before the first frame update
     void Start()
     {
-        
+        flags = GameController.cyberRoomItemFlags;
     }
 
     // Update is called once per frame
@@ -18,8 +21,18 @@ public class ItemController : MonoBehaviour
 
     public void OnClickItem(){
         string itemName = gameObject.name;
-        GameController.cyberRoomItemFlags[itemName] = true;
+        flags[itemName] = true;
         BagController.setItemImage();
         gameObject.SetActive(false);
     }
+
+    public void OnClickDoor(){
+        if(flags.ContainsKey("Key") && flags["Key"]){
+
+            } else {
+                ResultLog.SendFungusMessage("DoorLocked");
+            }
+    }
+
+
 }
